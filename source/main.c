7f2bcf743c98bd983bb9382110b02e2decb6ea90
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-05-25 17:36:50 -0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,36 +1,35 @@
-/**
- * @file    OneLed.c
- * @brief   Application entry point.
- */
-/**Standard input/output to debug console*/
 #include <stdio.h>
-/**Standard integer definition provided by the compiler*/
 #include <stdint.h>
 #include "MK64F12.h"
 
-
-
-
 int main(void) {
 
-	SIM->SCGC5 = 0x400;
-
+	/*SIM->SCGC5 = 0x400;
+	PORTB->PCR[21] = 0x00000100;
+	GPIOB->PDOR = 0x00200000;
+	GPIOB->PDDR = 0x00200000;
 	PORTB->PCR[22] = 0x00000100;
-
 	GPIOB->PDOR = 0x00400000;
-
-	GPIOB->PDDR = 0x00400000;
+	GPIOB->PDDR = 0x00400000;*/
+	SIM->SCGC5 = 0x2000;
+	PORTE->PCR[26] = 0x00000100;
+	GPIOE->PDOR = 0x004000000;
+	GPIOE->PDDR = 0x004000000;
 
     while(1) {
 
-		GPIOB->PDOR = 0;
-
+		/*GPIOB->PDOR = 0;
 		printf("RED LED ON\n");
-
-
 		GPIOB->PDOR= 0x00400000;
-
 		printf("RED LED OFF\n");
+		GPIOB->PDOR = 0;
+		printf("BLUE LED ON\n");
+		GPIOB->PDOR= 0x00200000;
+		printf("BLUE LED OFF\n");*/
+    	GPIOE->PDOR = 0;
+    	printf("GREEN LED ON\n");
+    	GPIOE->PDOR= 0x04000000;
+    	printf("GREEN LED OFF\n");
 
     }
     return 0 ;
