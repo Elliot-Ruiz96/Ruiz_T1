fb0ebe35914a3f97ac4a19c72a8bdc6fef70907c
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-05-25 16:47:12 -0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,49 +1,36 @@
-
+/**
+ * @file    OneLed.c
+ * @brief   Application entry point.
+ */
+/**Standard input/output to debug console*/
 #include <stdio.h>
-#include "fsl_clock.h"
-#include "fsl_gpio.h"
-#include "fsl_port.h"
-#include "fsl_common.h"
+/**Standard integer definition provided by the compiler*/
+#include <stdint.h>
+#include "MK64F12.h"
 
-gpio_pin_config_t led_config = {
-        kGPIO_DigitalOutput,
-        1,
-    };
 
-#define PIN22      	22u
-#define	PIN21		21u
-#define PIN26		26u
-#define CORE_FREQ	21000000u
-#define DELAY		1000000u
+
 
 int main(void) {
 
-	 CLOCK_EnableClock(kCLOCK_PortB);
-	 CLOCK_EnableClock(kCLOCK_PortE);
+	SIM->SCGC5 = 0x400;
 
-	 PORT_SetPinMux(PORTB, PIN22, kPORT_MuxAsGpio); // Red LED
-	 PORT_SetPinMux(PORTB, PIN21, kPORT_MuxAsGpio); // Blue LED
-	 PORT_SetPinMux(PORTE, PIN26, kPORT_MuxAsGpio); // Green LED
+	PORTB->PCR[22] = 0x00000100;
 
-	 GPIO_PinInit(GPIOB, PIN22, &led_config);
-	 GPIO_PinInit(GPIOB, PIN21, &led_config);
-	 GPIO_PinInit(GPIOE, PIN26, &led_config);
+	GPIOB->PDOR = 0x00400000;
 
+	GPIOB->PDDR = 0x00400000;
 
     while(1) {
 
-    	GPIO_PortClear(GPIOB, 1u << PIN21);
-    	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
-    	GPIO_PortSet(GPIOB, 1u << PIN21);
-    	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
-     	GPIO_PortClear(GPIOB, 1u << PIN22);
-     	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
-     	GPIO_PortSet(GPIOB, 1u << PIN22);
-     	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
-    	GPIO_PortClear(GPIOE, 1u << PIN26);
-    	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
-    	GPIO_PortSet(GPIOE, 1u << PIN26);
-    	SDK_DelayAtLeastUs(DELAY, CORE_FREQ);
+		GPIOB->PDOR = 0;
+
+		printf("RED LED ON\n");
+
+
+		GPIOB->PDOR= 0x00400000;
+
+		printf("RED LED OFF\n");
 
     }
     return 0 ;
